9f69daac1102d89041cfb461ac88159a076118b4
Rodolphe <dutchr@live.fr>
2021-08-03 23:21:43 +0200

    setup mongoDB

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const express = require('express'),
-app = express(),
-port = process.env.PORT || 3000,
-coursesController = require('./controllers/courseController'),
-router = express.Router();
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 3000,
+  coursesController = require("./controllers/courseController"),
+  router = express.Router();
+const mongoose = require("mongoose");
+const dbURL = "mongodb://localhost:27017/";
+const dbName = "kitchen_db";
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(dbURL + dbName, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`[Mongo connected !!]`))
+  .catch((err) => {
+    console.log(Error, err.message);
+  });
 
-
-app.set('view engine', 'pug');
-app.set('views', "./views")
-router.use(express.static('public'));
-router.use(express.urlencoded({ extended: false}));
+app.set("view engine", "pug");
+app.set("views", "./views");
+router.use(express.static("public"));
+router.use(express.urlencoded({ extended: false }));
 router.use(express.json());
 
-
-router.get('/courses', coursesController.index);
-router.get('/courses/new', coursesController.new);
-router.post('/courses/create', coursesController.create);
-router.get('/courses/:id', coursesController.show);
-router.get('/courses/:id/edit', coursesController.edit);
-router.put('/courses/:id/update', coursesController.update);
-router.delete('/courses/:id/delete', coursesController.delete);
-
-
+router.get("/courses", coursesController.index);
+router.get("/courses/new", coursesController.new);
+router.post("/courses/create", coursesController.create);
+router.get("/courses/:id", coursesController.show);
+router.get("/courses/:id/edit", coursesController.edit);
+router.put("/courses/:id/update", coursesController.update);
+router.delete("/courses/:id/delete", coursesController.delete);
 
 app.use("/", router);
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });

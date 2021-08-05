e71527c959a66123227d978ba9ab568aa412fd68
LittleFootDev <thelittlefootdev@gmail.com>
2021-08-05 16:14:36 +0200

    adding controller  subscriber

---
+++ controllers/subsciberController.js
@@ -0,0 +1,87 @@
+const Subscriber = require("../models/subscriber");
+
+const getSubscriberParams = (body) => {
+  return {
+    name: body.name,
+    email: body.email,
+    zipCode: parseInt(body.zipCode),
+  };
+};
+
+exports.index = (req, res, next) => {
+  Subscriber.find()
+    .then((subscribers) => {
+      res.render("subscribers/index", { subscribers: subscribers });
+      next();
+    })
+    .catch((err) => {
+      console.log(`Error fetching subscribers: ${err.message}`);
+      next(err);
+    });
+};
+
+exports.new = (req, res) => {
+  res.render("subscribers/new");
+};
+
+exports.create = (req, res, next) => {
+  let newSubscriber = getSubscriberParams(req.body);
+  Subscriber.create(newSubscriber)
+    .then((subscriber) => {
+      subscriber;
+      next();
+    })
+    .catch((err) => {
+      console.log(`Error saving new subscriber ${err.message}`);
+      next(err);
+    });
+};
+
+exports.show = (req, res, next) => {
+  let subscriberId = req.params.id;
+  Subscriber.findById(subscriberId)
+    .then((subscriber) => {
+      subscriber;
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error fetching subscriber by ID :${error.message}`);
+      next(error);
+    });
+};
+exports.edit = (req, res, next) => {
+  let subscriberId = req.params.id;
+  Subscriber.findById(subscriberId)
+    .then((subscriber) => {
+      res.render("subscribers/edit", { subscriber: subscriber });
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error fetching subscriber by ID :${error.message}`);
+      next(error);
+    });
+};
+exports.update = (req, res, next) => {
+  let subscriberId = req.params.id;
+  Subscriber.findByIdAndUpdate(subscriberId, { $set: req.body })
+    .then((subscriber) => {
+      res.render(`subscribers/${subscriberId}`, { subscriber: subscriber });
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error fetching subscriber by ID :${error.message}`);
+      next(error);
+    });
+};
+exports.delete = (req, res, next) => {
+  let subscriberId = req.params.id;
+  Subscriber.findByIdAndRemove(subscriberId, { $set: req.body })
+    .then(() => {
+      res.redirect("/subscribers");
+      next();
+    })
+    .catch((error) => {
+      console.log(`Error deleting subscriber by ID :${error.message}`);
+      next();
+    });
+};
